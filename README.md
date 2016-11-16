7ffba19f6ffa91276bb1010b23de1327cd6d27e5
zamicol <zamicol@gmail.com>
2016-11-16 00:34:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# jsonflags
+See the [godoc](https://godoc.org/github.com/zamicol/jsonflags) for eveything you need to know.  
+
+jsonflags is a simple package to use JSON in conjunction with Go's flag package.
+
+
+Example Go code:
+```
+type ExampleConfig struct {
+	Flag1 string
+	Flag2 string
+	Flag3 int
+}
+
+func main(){
+  var config ExampleConfig
+  flag.StringVar(&config.Flag1, "flag1", "defaultFlag1", "flag1Desc")
+  flag.StringVar(&config.Flag2, "flag2", "defaultFlag2", "flag2Desc")
+  flag.IntVar(&config.Flag3, "flag3", 1, "flag3Desc")
+  jsonflags.Parse(&config)
+}
+```
+
+Example json file:
+```
+{
+    "flag1": "jsonFlag1",
+    "flag2": "jsonFlag2",
+    "flag3": 3,
+}
+```
---
+++ jsonflags.go
@@ -0,0 +1,61 @@
+// Package jsonflags is a simple example of how to use JSON configs in
+// conjunction with Go's flag package.
+//
+// This package uses Go's json package for decoding.  The json decoder only
+// has accesses the exported fields of struct types and follows it's own
+// precedence for decoding, namely tags, exact case, and non-case sensitive.
+//
+// Flag values do not need to appear in the config file
+// and extra config file values will be ignore.
+//
+// Order of Precedence for defined values
+//    1. Command-line flags. (cli Example: `--flag1=flag1Value`)
+//    2. JSON values (json Example: `{"flag2": "flag2Value"}`)
+//    3. Default flag values (go Example: `flag.StringVar(&config.Flag3, "flag3", "flag3Value", "flag3Desc")`)
+//
+// Recommended Usage
+//    1. Define a config struct with fields.
+//    2. Use flag's functions to set default config values such as `flag.StringVar(&config.Flag1, "flag1", "defaultFlag1value", "flag1Desc")`
+//    3. Put configs in a `config.json` file. (or use --config=foobar to point somewhere else)
+//    4. Call `jsonflags.Parse(&config)`
+// The config struct is now appropriately populated.
+//
+// Testing
+//
+// Since this package uses flag, test functions need a cli flag passed to verify
+// cli parsing is working.
+//    go test --flag1=paramFlag1
+//
+package jsonflags
+
+import (
+	"encoding/json"
+	"flag"
+	"os"
+)
+
+// flag for config path
+var config = flag.String("config", "config.json", "Path to json config file.")
+
+// Parse reads config file and parses cli flags into c by calling flag.Parse()
+func Parse(c interface{}) {
+	parseJSON(*config, c)
+	flag.Parse()
+}
+
+func parseJSON(configPath string, c interface{}) {
+	if *config == "" {
+		return
+	}
+	file, err := os.Open(configPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(c)
+	if err != nil {
+		panic(err)
+	}
+}
